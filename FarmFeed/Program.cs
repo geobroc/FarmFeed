7e2e902db5973cc42c156f85cd7842a03baa7f54
Georgia Brockie <geobroc@chbc.school.nz>
2024-05-24 09:16:06 +1200

    Add project files.

---
+++ FarmFeed/Program.cs
@@ -0,0 +1,108 @@
+﻿// imports
+using System;
+using System.Collections.Generic;
+
+namespace FarmFeeding
+{
+    class Program
+    {
+        // Global variables
+        List<string> species = new List<string>() { "Romeney", "KuneKune", "Angus", "Leghorn" };
+        List<string> food = new List<string>() { "300g", "2500g", "10886g", "125g" };
+        static int Flag = 0;
+
+        // Methods and/or function
+
+        // Check to see if an interger enter by a user is within range
+        static int CheckSpecies(string question, int min, int max)
+        {
+            Console.WriteLine(question);
+
+            while (true)
+            {
+                try
+                {
+
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInt >= min && userInt <= max)
+                    {
+                        return userInt;
+                    }
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max}\n");
+
+                }
+
+                catch
+                {
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max}\n");
+                }
+            }
+        }
+        static int CheckFood(string question, int min, int max)
+        {
+            Console.WriteLine(question);
+
+            while (true)
+            {
+                try
+                {
+
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInt >= min && userInt <= max)
+                    {
+                        return userInt;
+                    }
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max}\n");
+
+                }
+
+                catch
+                {
+                    Console.WriteLine($"Error: Please enter a number between {min} and {max}\n");
+                }
+            }
+        }
+
+
+        static void FarmFeeding()
+        {
+            // Decide which species is used
+
+            int species = CheckSpecies("Choose a species:\n" +
+               "1. Romney\n" +
+               "2. KuneKune\n" +
+               "3. Angus\n" +
+               "4. Leghorn\n" +
+               "5. Quit\n", 1, 5);
+
+            if (species == 5)
+            {
+                Flag = 5;
+            }
+
+            Console.WriteLine($"You chose {species}. Are you sure to continue, press Enter.");
+            Console.ReadLine();
+            Console.Clear();
+
+            int food = CheckFood("Choose your amount of food:\n" +
+                "1. 300g\n" +
+                "2. 2500g\n" +
+                "3. 10886g\n" +
+                "4. 125g\n", 1, 4);
+
+            Console.WriteLine($"Species breed {species} eats {food} amount of food.");
+
+        }
+
+        static void Main()
+        {
+            while (Flag != 5)
+            {
+                FarmFeeding();
+            }
+        }
+    }
+
+}
